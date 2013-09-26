d51233fdb3cd09ab5632140a48c4f9cda840f344
Konstantine <kosyak.dm@mail.ru>
2013-09-26 15:25:35 +0400

    Created project and database. Implemented logic for author entities.

---
+++ TheLibrary/DAL/AuthorService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLibrary.DAL
+{
+    class AuthorService
+    {
+        public IEnumerable<Author> GetAllAuthors()
+        {
+            List<Author> result;
+            using (var context = new TheLibraryEntities())
+            {
+                result = context.Authors.ToList();
+            }
+            return result;
+        }
+
+        public IEnumerable<Author> GetPopularInYear(int year)
+        {
+            IEnumerable<Author> result;
+            using (var context = new TheLibraryEntities())
+            {
+                result = (from author in context.Authors
+
+                          select new
+                          {
+                              Author = author,
+                              PopularityIndex = (from book in author.Books
+                                                 select (from issuance in book.BookIssuances
+                                                         where issuance.IssuanceDate.Year == year
+                                                         select issuance).Count()
+                                                 ).Sum()
+                          }).Where(a => a.PopularityIndex > 0)
+                          .OrderByDescending(a => a.PopularityIndex)
+                          .Select(a => a.Author).ToList();
+            }
+            return result;
+
+        }
+    }
+}
